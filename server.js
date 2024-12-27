cd5ea4e94a25bedc04d0025b801237eed772f0b7
mohammadreza-2024 <mohammadrezaian2020@gmail.com>
2024-12-27 21:33:51 +0330

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'src', 'db.json'));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(4000, () => {
+  console.log('JSON Server is running on port 3000');
+});
